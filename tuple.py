e23b87eccc4002be65876f4011a10e1833edbc82
arunkumardasa <arunkumar.csit9@gmail.com>
2024-11-22 17:06:58 +0530

    adding my tuple file

---
+++ tuple.py
@@ -0,0 +1,11 @@
+t = (5,9,8,7)
+t1 = t + (11,12,45,98)
+
+print(t,t1)
+print(t1.index(5))
+print("Count of the value:",t.count(8))
+print("Sorted tuple:",sorted(t1))
+print("original tuple:",t1)
+
+t3 = ((1,5,6),) *5
+print(t3)
